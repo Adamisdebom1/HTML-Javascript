f1addfb83b246258c2b83e14f5cb4a653275de83
Adamisdebom1 <aaltorf@roc-dev.com>
2017-11-16 10:59:55 +0100

    klaar opdracht 1

--- nieuwjavascript.js
+++ nieuwjavascript.js
@@ -1,8 +1,8 @@
-var a = 10;
-if(a > 9) {
-    console.log("Hallo a is groter dan 9 hoor");
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
 }
 
-for(var i=0; i < 10; i++) {
-    console.log("i heeft nu de waarde" + i);
-}
+console.log(mijnauto.kleur);
